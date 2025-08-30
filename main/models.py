9e8f1de8567958f2f521ef72d9fdd4c8da659897
Maxim <you@example.com>
2025-08-30 11:02:44 +0200

    add custom home page

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField("zagpolovok",max_length=100)
+    text = models.TextField()
+    author = models.CharField("avtor",max_length=100)
+    date = models.DateField()
+
+    class Meta:
+        verbose_name = "zapis"
+        verbose_name_plural = "zapisi"
+
+    def __str__(self):
+        return f'{self.title},{ self.author}'
