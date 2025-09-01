aa21b97e92a9da08537642c6186a12ed950aa7a1
Maxim <you@example.com>
2025-09-01 04:39:02 +0200

    Add Post model with admin registration

--- build.sh
+++ build.sh
@@ -2,28 +2,19 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+pip #!/usr/bin/env bash
+set -o errexit
 
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Создание суперпользователя ПОСЛЕ миграций
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword123')
-    print('Superuser created')
+    User.objects.create_superuser('admin', 'admin@example.com', '123456')
+    print('Superuser created with password: 123456')
 else:
     print('Superuser already exists')
-"
-
-# Отладка - покажет созданных пользователей
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-users = User.objects.all()
-print(f'Total users: {users.count()}')
-for user in users:
-    print(f'Username: {user.username}, Email: {user.email}, is_superuser: {user.is_superuser}')
 "
