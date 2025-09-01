2561d692fd0d6c5c34998a3f1f38ff2087f79abc
Maxim <you@example.com>
2025-09-01 05:08:37 +0200

    Add Post model with admin registration

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip #!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
+
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
 
