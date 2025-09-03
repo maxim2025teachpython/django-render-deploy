6b6ebc5122ea4b4be91f84ec4454831879c0dd72
Maxim <you@example.com>
2025-09-03 02:20:31 +0200

    search field

--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.db.models import Q
 from .models import Post
 
 class PostView(View):
-
-    def get(self,request):
-        posts = Post.objects.all()
-        return render(request, 'home.html', {'posts_list':posts})
+    def get(self, request):
+        query = request.GET.get('query', '')
+        if query:
+            posts = Post.objects.filter(
+                Q(title__icontains=query) | Q(text__icontains=query)
+            )
+        else:
+            posts = Post.objects.all()
+        return render(request, 'home.html', {
+            'posts_list': posts,
+            'query': query
+        })
