734a84e541b2247259900e13348177c6ab82e1ab
Maxim <you@example.com>
2025-09-06 06:56:22 +0200

    views

--- config/settings.py
+++ config/settings.py
@@ -96,3 +96,17 @@ if RENDER_EXTERNAL_HOSTNAME:
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
